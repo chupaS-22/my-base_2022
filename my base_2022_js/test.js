f2fdb5a27c51104921030ff64c5053fb828a3390
chupaS-22 <sancholamancho@yandex.ru>
2022-11-28 18:36:35 +0300

    mb_28_11-18-36

--- my base_2022_js/test.js
+++ my base_2022_js/test.js
@@ -1,2 +1,17 @@
 
+//Метки
+
+firstFor: for (let num = 0; num < 2; num++) {
+	for (let size = 0; size < 3; size++) {
+		if (size == 2) {
+			continue firstFor;
+		}
+		console.log(size);
+	}
+}
+
+
+
+
+
 
