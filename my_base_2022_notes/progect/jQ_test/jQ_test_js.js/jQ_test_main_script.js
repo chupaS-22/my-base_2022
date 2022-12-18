2fdd2a6082a39608b00463a2f61ab2922e0c9f9f
chupaS-22 <sancholamancho@yandex.ru>
2022-12-18 16:27:51 +0300

    mb_18_12_16-28

---
+++ my_base_2022_notes/progect/jQ_test/jQ_test_js.js/jQ_test_main_script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+//здесь пишем наш код, который должен будет
+//выполняться, только после загрузки web-страницы
+});
+jQuery(document).ready(function(){
+	$('.first').css({'color':'red'});
+});
