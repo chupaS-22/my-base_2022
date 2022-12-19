f8c61ec295becd7f068eae802c679b2fefa617c4
chupaS-22 <sancholamancho@yandex.ru>
2022-12-20 00:10:40 +0300

    mb_20_12_00-11

--- my_base_2022_notes/progect/jQ_test/jQ_test_js.js/jQ_test_main_script.js
+++ my_base_2022_notes/progect/jQ_test/jQ_test_js.js/jQ_test_main_script.js
@@ -1,7 +1,15 @@
-jQuery(document).ready(function(){
+//jQuery(document).ready(function(){
 //здесь пишем наш код, который должен будет
 //выполняться, только после загрузки web-страницы
-});
-jQuery(document).ready(function(){
-	$('.first').css({'color':'red'});
-});
+//});
+//jQuery(document).ready(function(){
+//	$('.first').css({'color':'red'});
+//});
+
+
+
+function getDistance(speed, time) {
+	let distance = speed * time;
+	return distance;
+	}
+	let myDistance = getDistance(10, 5);
